daeba118c9ec0db00f2230d2b79acda3c5531160
Kalle Korhonen <kalle.korhonen@quuppa.com>
2025-12-25 09:47:03 +0000

    Add room detection capabilities, enhance opening/door capabilities by connecting them to rooms of which they are in between

--- src/detection/__init__.py
+++ src/detection/__init__.py
@@ -1,1 +1,18 @@
 # Detection components
+from src.detection.room_detector import (
+    RoomDetector,
+    Room,
+    Contour,
+    FloorplanContour,
+    RoomType
+)
+from src.detection.opening_connection_analyzer import (
+    OpeningConnectionAnalyzer,
+    OpeningConnection,
+    OpeningType,
+    ConnectionType
+)
+from src.detection.floorplan_analyzer import (
+    FloorplanAnalyzer,
+    FloorplanAnalysisResult
+)
